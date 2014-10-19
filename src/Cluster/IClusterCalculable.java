32b78324a4872e1a8c78dd7c2972dfad602c4dd0
Jayveehe <jayveehe@gmail.com>
2014-10-19 21:41:46 +0800

    初始提交，包含了K-Means和AP聚类，主界面UI程序

---
+++ src/Cluster/IClusterCalculable.java
@@ -0,0 +1,43 @@
+package Cluster;
+
+/**
+ * Created by Jayvee on 2014/9/18.
+ */
+public interface IClusterCalculable<D> {
+    /**
+     * 获取待计算的向量值
+     *
+     * @return
+     */
+    public float[] getVecValues();
+
+    /**
+     * 获取向量ID
+     *
+     * @return
+     */
+    public int getID();
+
+    /**
+     * 获取向量类别ID
+     *
+     * @return
+     */
+    public int getTypeID();
+
+    /**
+     * 设置向量类别ID
+     *
+     * @param typeID
+     */
+    public void setTypeID(int typeID);
+
+    public void setVecValues(float[] vecValues);
+
+    public void setID(int ID);
+
+//    public void setObj(D obj);
+
+    public D getObj();
+
+}
